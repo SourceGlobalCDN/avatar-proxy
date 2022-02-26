c7350487cfc18369e6e7e347f793fc5ce597391f
AH-dark <ahdark@outlook.com>
2022-02-26 13:41:53 +0800

    Finished 1.0.0 version

--- src/index.js
+++ src/index.js
@@ -1,41 +1,88 @@
-'use strict';
+"use strict";
 
-const Reg = new RegExp("^(/avatar|/gravatar)?/[a-zA-Z0-9]{32}")
+const axios = require("axios");
+
+const ImageReg = new RegExp("^(/avatar|/gravatar)/[a-zA-Z0-9]{32}");
+const InfoReg = new RegExp("^/[a-zA-Z0-9]{32}\\.(json|xml|php|vcf|qr)");
 
 exports.main_handler = async (event, context) => {
     const path = event.path;
-    if(path.startsWith("/")) {
+    if (path === "/") {
         return {
             isBase64Encoded: false,
             statusCode: 301,
             headers: {
-                "Content-Type":"text/plain",
-                "Location": "https://www.sourcegcdn.com"
+                "Content-Type": "text/plain",
+                Location: "https://www.sourcegcdn.com",
             },
-            body: "301 Permanently Moved"
-        }
+            body: "301 Permanently Moved",
+        };
     }
-    if(!Reg.test(path)) {
+    if (!ImageReg.test(path) && !InfoReg.test(path)) {
         return {
             isBase64Encoded: false,
             statusCode: 404,
             headers: {
-                "Content-Type":"text/plain"
+                "Content-Type": "text/plain",
             },
-            body: "file not found, incorrect path"
-        }
+            body: "file not found, incorrect path",
+        };
     }
-    if(!path.startsWith("/avatar")) {
+    if (path.startsWith("/gravatar")) {
         return {
             isBase64Encoded: false,
             statusCode: 302,
             headers: {
-                "Content-Type":"text/plain",
-                "Location": "/avatar"+path,
-                "X-Info": "Please use a url starting with /avatar/"
+                "Content-Type": "text/plain",
+                Location: path.replace("/gravatar/", "/avatar/"),
+                "X-Info": "Please use a url starting with /avatar/",
             },
-            body: "302 Moved"
-        }
+            body: "302 Moved",
+        };
     }
-    
+    let res;
+    try {
+        res = await axios("https://www.gravatar.com" + path, {
+            method: event.httpMethod.toUpperCase(),
+            params: {
+                s: event.queryString.hasOwnProperty("s")
+                    ? event.queryString.s
+                    : undefined,
+                d: event.queryString.hasOwnProperty("d")
+                    ? event.queryString.d
+                    : undefined,
+                f: event.queryString.hasOwnProperty("f")
+                    ? event.queryString.s
+                    : undefined,
+                r:
+                    event.queryString.hasOwnProperty("s") &&
+                    (event.queryString.s === "g" ||
+                        event.queryString.s === "pg")
+                        ? event.queryString.s
+                        : "pg",
+            },
+            responseType: "arraybuffer",
+        });
+    } catch (err) {
+        console.error("[Axios]", err);
+        return {
+            isBase64Encoded: false,
+            statusCode: 404,
+            headers: {
+                "Content-Type": "text/plain",
+                "X-Info": "Axios error",
+            },
+            body: "404 Not Found",
+        };
+    }
+
+    return {
+        isBase64Encoded: true,
+        statusCode: 200,
+        headers: {
+            "Content-Type": res.headers["content-type"],
+            "X-Info": "Success",
+        },
+        body: Buffer.from(res.data, "binary").toString("base64"),
+    };
 };
