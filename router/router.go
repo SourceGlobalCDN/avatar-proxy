8b1a1fd67f304d9aab65161e96e516b47ea198ae
AH-dark <ahdark@outlook.com>
2022-09-22 21:37:20 +0800

    Feat: Homepage Controller

--- router/router.go
+++ router/router.go
@@ -1,9 +1,14 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/SourceGlobalCDN/avatar-proxy/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.GET("/", controller.HomepageHandler)
 
 	return r
 }
